24aa0f9074f780dd7be03af4cfd41e7ec3192a9c
Daniel McBryde <mcbryded@gmail.com>
2016-04-16 11:57:51 -0500

    make reminders route

--- app/router.js
+++ app/router.js
@@ -2,12 +2,13 @@ import Ember from 'ember';
 import config from './config/environment';
 
 const Router = Ember.Router.extend({
-  location: config.locationType
+  location: config.locationType,
 });
 
 Router.map(function() {
-  this.route('lists', {path:'/'}, function() {
-    this.route('new', {path:'/new-list'});
+  this.route(`lists`, { path: `/` }, function() {
+    this.route(`new`, { path: `/new-list` });
+    this.route(`reminders`, { path: `/reminders` });
   });
 });
 
